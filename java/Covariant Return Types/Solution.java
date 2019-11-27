e5856b6222896f16d70c4aa9287fba7e75d12e26
Uladzimir Karalchuk <korolchuk1986@gmail.com>
2019-11-27 21:27:26 +0100

    JAVA - Covariant Return Types

--- java/Covariant Return Types/Solution.java
+++ java/Covariant Return Types/Solution.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-//Complete the classes below
 class Flower {
     String whatsYourName(){
         return "Flower";
