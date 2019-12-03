1833329d6015646c518b54bda52b0c3644c5e70c
Uladzimir Karalchuk <korolchuk1986@gmail.com>
2019-12-03 22:39:08 +0100

    JAVA - Problem Solving

---
+++ Problem Solving/CamelCase/Solution.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the camelcase function below.
+    static int camelcase(String s) {
+        char[] arr = s.toCharArray(); 
+        int count = 0;
+        for (char ch : arr) {
+            int ascii = (int) ch;
+
+            if  (41 <= ascii && ascii <= 90 ) {
+                count++;
+            }
+        }
+        return ++count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        int result = camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
+
