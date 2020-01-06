562c4e810e46388b8384fd21161700a11be333b1
Uladzimir KARALCHUK <u.karalchuk@FIDUCIAL.DOM>
2020-01-06 17:08:17 +0100

    JAVA - Picking Numbers

---
+++ Problem Solving/Picking Numbers/Solution.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<Integer> a) {
+    // Write your code here
+        int res = 0;
+        for(int i = 0; i < a.size(); i++) {
+            int tmp = 0;
+            for(int j = 0; j < a.size(); j++) {
+                if(a.get(i) - a.get(j) == 1 || a.get(i) - a.get(j) == 0) {
+                    tmp++;
+                }
+            }
+            if (res < tmp) {
+                res = tmp;
+            }
+        }
+
+        return res;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] aTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aTemp[i]);
+            a.add(aItem);
+        }
+
+        int result = Result.pickingNumbers(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
